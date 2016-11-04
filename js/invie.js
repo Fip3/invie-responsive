b873426836c1927ffc98735f02ffd908f5049064
Felipe Torrejon <felipetorrejon@live.cl>
2016-11-04 02:13:30 -0300

    clase10 - usando media queries en javascript

--- js/invie.js
+++ js/invie.js
@@ -1,7 +1,20 @@
+var consulta = window.matchMedia('(max-width: 500px)');
 var $burgerButton = document.getElementById('burger-button');
 var $menu = document.getElementById('menu');
+
+consulta.addListener(mediaQuery);
+
 $burgerButton.addEventListener('touchstart', toggleMenu);
+
+function mediaQuery(){
+  if (consulta.matches) {
+    $burgerButton.classList.remove('hidden');
+  } else {
+    $burgerButton.classList.add('hidden');
+  };
+}
 
 function toggleMenu() {
   $menu.classList.toggle('active');
+  $burgerButton.classList.toggle('icon-close');
 }
